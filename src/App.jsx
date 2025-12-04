79f46113b7471ef7c3904bd1283a986145b11fbd
ariful4082 <ariful4082@gmail.com>
2025-12-04 08:09:34 +0600

    project choice and reset turn with run ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { use, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
@@ -23,33 +23,84 @@ const cardImages = [
 
 function App() {
   const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
   // shuffle cards with added id
   const shuffleCards = ()=>{
     const shuffled = [...cardImages, ...cardImages].sort(()=> Math.random() - 0.5).map((card)=> ({...card, id: Math.random()}));
     setCards(shuffled);
+    setTurns(0);
+    setChoiceOne(null);
+    setChoiceTwo(null);
   };
 
   useEffect(()=>{
     shuffleCards();
   }, []);
 
+  // handle a choice
+  const handleChoice = (card)=>{
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  // reset choices & increase turn
+
+  const resetTurn = ()=>{
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns(prev=> prev + 1);
+    setDisabled(false);
+  };
+
+  useEffect(()=>{
+    if(choiceOne && choiceTwo){
+      setDisabled(true);
+
+      if(choiceOne.src === choiceTwo.src){
+
+        // matched shoud
+
+        setCards(prev=>{
+          return prev.map(card=>{
+            if(card.src === choiceOne.src){
+              return {...card, matched: true}
+            }else{
+              return card;
+            }
+          })
+        });
+        resetTurn();
+      }else{
+        setTimeout(()=>resetTurn(), 1000);
+      }
+    }
+
+
+  },[choiceOne, choiceTwo]);
+
   return (
     <>
       <div className='min-h-screen flex flex-col items-center py-10 transition duration-300'>
           <h1 className='text-3xl font-bold mb-4 dark:text-white text-green-600'>Memory Card Game</h1>
+
+          <button onClick={shuffleCards} className='mb-6 px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition duration-300'>New Game</button>
+          <div className='text-2xl'>Turns: {turns}</div>
           <div className='grid grid-cols-4 gap-2 w-[350px] sm:w-[500px]'>
             {
               cards.map((card)=>(
                 <div key={card.src}>
                   <div
-                    className='relative w-full h-30 sm:h-45 cursor-pointer transform transition-transform duration-300'
+                    onClick={()=> !disabled && handleChoice(card)}
+                    className={`relative w-full h-30 sm:h-45 cursor-pointer transform transition-transform duration-300 ${ (card === choiceOne || card === choiceTwo || card.matched) ? 'rotate-y-180' : '' }`}
                   >
                     <img src={card.src} alt="Card Image"
-                      className='absolute inset-0 w-full h-full rounded-lg backface-hidden'
+                      className={`absolute inset-0 w-full h-full rounded-lg backface-hidden ${ (card === choiceOne || card === choiceTwo || card.matched) ? 'opacity-100' : 'opacity-0' }`}
                     />
 
-                    <div className='absolute inset-0 w-full h-full rounded-lg bg-blue-500 flex items-center justify-center text-white text-2xl font-bold'>
+                    <div className={`absolute inset-0 w-full h-full rounded-lg bg-blue-500 flex items-center justify-center text-white text-2xl font-bold backface-hidden ${ (card === choiceOne || card === choiceTwo || card.matched) ? 'opacity-0' : 'opacity-100' }`}>
                       ?
                     </div>
                   </div>
